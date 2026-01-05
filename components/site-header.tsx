33e5bd77f0acadb4632865d8b0412bb67d9ce9b7
G.PAUDRA <g.paudra@gmail.com>
2026-01-05 21:29:32 +0700

    Update site-header.tsx
    update ukuran font

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -14,7 +14,7 @@ export function SiteHeader() {
           className="mx-2 data-[orientation=vertical]:h-4"
         />
         {/* <h1 className="text-base font-medium">Documents</h1> */}
-        <h1 className="text-base font-medium"><AmbilTanggal/></h1>
+        <h1 className="text-lg font-medium"><AmbilTanggal/></h1>
         <div className="ml-auto flex items-center gap-2">
           {/* <Button variant="ghost" asChild size="sm" className="hidden sm:flex">
             <a
