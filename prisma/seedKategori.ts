f74a3a31a216efdcd632c5fdea0a32726d4e183b
G.PAUDRA <g.paudra@gmail.com>
2026-01-06 17:49:08 +0700

    update seed kategori
    ganti pakai prisma lib

--- prisma/seedKategori.ts
+++ prisma/seedKategori.ts
@@ -2,10 +2,7 @@
 
 // import keperluan
 
-import { PrismaClient } from "@/generated/prisma/client";
-
-
-const prisma = new PrismaClient({} as any)
+import {prisma} from '@/lib/prisma'
 
 async function main() {
     try{
