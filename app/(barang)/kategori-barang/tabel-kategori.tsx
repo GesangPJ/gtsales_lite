99d252c8ec8f1891fadc68966a80fbad7b7b08de
G.PAUDRA <g.paudra@gmail.com>
2026-01-07 18:58:42 +0700

    buat komponen tabel kategori

---
+++ app/(barang)/kategori-barang/tabel-kategori.tsx
@@ -0,0 +1,42 @@
+// Komponen tabel kategori barang
+
+import { prisma } from "@/lib/prisma"
+import { columns } from "./column"
+import { DataTable } from "@/components/data-table"
+
+type Kategori = {
+  id: number
+  nama_kategori: string
+  status: string
+}
+
+async function getKategoris(): Promise<Kategori[]> {
+  try {
+    return await prisma.kategori.findMany({
+      select: {
+        id: true,
+        nama_kategori: true,
+        status: true,
+      },
+    })
+  } catch (error) {
+    console.error("Data Kategori tidak dapat diambil", error)
+    return []
+  }
+}
+
+export default async function TabelKategori() {
+
+    const kategoris = await getKategoris()
+
+    return(
+        <div>
+            <DataTable
+            columns={columns}
+            data={kategoris}
+            />
+        </div>
+    )
+}
+
+
