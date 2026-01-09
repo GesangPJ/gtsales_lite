34e85da2e06b33ac219f3ab136c1d596f5ceeb11
G.PAUDRA <g.paudra@gmail.com>
2026-01-09 19:05:52 +0700

    halaman tabel barang

---
+++ app/(barang)/data-barang/page.tsx
@@ -0,0 +1,42 @@
+import { AppSidebar } from "@/components/app-sidebar"
+import { SiteHeader } from "@/components/site-header"
+import {
+  SidebarInset,
+  SidebarProvider,
+} from "@/components/ui/sidebar"
+
+import TabelBarang from "./tabel-barang"
+
+export default function HalamanDataBarang() {
+  return (
+    <SidebarProvider
+      style={
+        {
+          "--sidebar-width": "calc(var(--spacing) * 72)",
+          "--header-height": "calc(var(--spacing) * 12)",
+        } as React.CSSProperties
+      }
+    >
+      <AppSidebar variant="inset" />
+      <SidebarInset>
+        <SiteHeader />
+        <div className="flex flex-1 flex-col">
+            
+          <div className="@container/main flex flex-1 flex-col gap-2">
+            
+            <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+                <div className="px-4 lg:px-6">
+                    <h1 className="text-2xl font-bold">Tabel Barang</h1>
+                </div>
+             
+              <div className="px-4 lg:px-6">
+                <TabelBarang/>
+              </div>
+              
+            </div>
+          </div>
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
+  )
+}
