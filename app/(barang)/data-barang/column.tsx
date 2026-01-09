bd261b9e73a3166d893e2cb369de373369971a19
G.PAUDRA <g.paudra@gmail.com>
2026-01-09 19:05:41 +0700

    buat komponen kolom barang

---
+++ app/(barang)/data-barang/column.tsx
@@ -0,0 +1,124 @@
+"use client"
+
+import { ColumnDef } from "@tanstack/react-table"
+import { Button } from "@/components/ui/button"
+import { ArrowUpDown} from "lucide-react"
+
+type Barang = {
+  id: number
+  nama_barang: string,
+  harga_jual: number,
+  harga_beli: number,
+  stok: number,
+  namaKategori?: string,
+  kadaluarsa:string,
+  barcode: number,
+}
+
+export const columns: ColumnDef<Barang>[] = [
+  {
+    accessorKey: "id",
+    size: 10,
+    header: "ID",
+    
+  },
+  {
+    accessorKey: "nama_barang",
+    size:50,
+    header: ({ column }) => (
+      < Button variant="ghost" onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        className="h-8 px-2"
+      >
+        Nama Barang
+        <ArrowUpDown className="ml-2 h-4 w-4" />
+      </Button>
+    ),
+    cell: ({ row }) => (
+      <div className="capitalize">
+        {row.getValue("nama_barang")}
+      </div>
+    ),
+  },
+  {
+    accessorKey: "harga_jual",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          className="text-left"
+        >
+          Harga Jual
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      )
+    },
+    cell: ({ row }) => {
+      const harga = parseFloat(row.getValue("harga_jual"))
+      
+      // Format ke Rp
+      const formatted = new Intl.NumberFormat("id-ID", {
+        style: "currency",
+        currency: "IDR",
+        minimumFractionDigits: 0,  // Hilangkan .00
+      }).format(harga)
+
+      return <div className="text-left font-medium">{formatted}</div>
+    },
+  },
+  {
+    accessorKey: "harga_beli",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Harga Beli
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      )
+    },
+    cell: ({ row }) => {
+      const harga = parseFloat(row.getValue("harga_beli"))
+      
+      // Format ke Rp
+      const formatted = new Intl.NumberFormat("id-ID", {
+        style: "currency",
+        currency: "IDR",
+        minimumFractionDigits: 0,  // Hilangkan .00
+      }).format(harga)
+
+      return <div className="text-left font-medium">{formatted}</div>
+    },
+  },
+  {
+    accessorKey: "namaKategori",
+    size:30,
+    header: ({ column }) => (
+      < Button variant="ghost" onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        className="h-8 px-2"
+      >
+        Kategori
+        <ArrowUpDown className="ml-2 h-4 w-4" />
+      </Button>
+    ),
+    cell: ({ row }) => (
+      <div className="capitalize">
+        {row.getValue("namaKategori")}
+      </div>
+    ),
+  },
+  {
+    accessorKey: "stok",
+    header: "Stok",
+  },
+  {
+    accessorKey: "barcode",
+    header: "Barcode",
+  },
+  {
+    accessorKey: "kadaluarsa",
+    header: "Kadaluarsa",
+  },
+]
