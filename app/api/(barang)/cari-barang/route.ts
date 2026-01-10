3b7945a55ccdc31a8d03c8c39a9b96cc985ed7a1
G.PAUDRA <g.paudra@gmail.com>
2026-01-10 20:44:39 +0700

    API untuk cari barang

---
+++ app/api/(barang)/cari-barang/route.ts
@@ -0,0 +1,37 @@
+// API ambil barang untuk POS
+
+import { prisma } from "@/lib/prisma"
+import { NextResponse, NextRequest } from "next/server"
+
+export async function GET(req: NextRequest) {
+  const query = req.nextUrl.searchParams.get('q')
+
+  if (!query || query.length < 2) {
+    return NextResponse.json([], { status: 400 })
+  }
+  
+  try {
+    const barangs = await prisma.barang.findMany({
+      where: {
+        nama_barang: {
+          contains: query,  // LIKE '%laptop%'
+        }
+      },
+      select: {
+        id: true,
+        nama_barang: true,
+        harga_jual: true,
+      },
+      take: 10  // Max 10 hasil
+    })
+
+    return NextResponse.json(barangs, { status: 200 })
+  } catch (error: unknown) {
+    console.error("Data Barang tidak dapat diambil", error)
+
+    return NextResponse.json(
+      { error: "Terjadi kesalahan saat mengambil data barang" },
+      { status: 500 }
+    )
+  }
+}
