f399262747800f3416167b2184cb4265c38ac535
G.PAUDRA <g.paudra@gmail.com>
2026-01-12 14:47:37 +0700

    persiapan halaman status pembelian

--- app/api/(pembelian)/status-pembelian/page.tsx
+++ app/api/(pembelian)/status-pembelian/page.tsx
@@ -1,5 +1,29 @@
-// Halaman Status Pembelian Barang
+// API Status Pembelian Barang
+import { prisma } from "@/lib/prisma"
+import { NextResponse } from "next/server"
+import type { NextRequest } from "next/server"
 
+// Ambil status pembelian
+
+export async function GET(_req: NextRequest){
+
+    try{
+        const statuspembelian = await prisma.pembelian.findMany({
+            select:{
+                id:true,
+                kode:true,
+                status:true,
+                nama_vendor:true,
+                jumlahtotalharga:true,
+            }
+
+        })
+
+    }catch(error){
+
+    }
+
+}
 
 
 
