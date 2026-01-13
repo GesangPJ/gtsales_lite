69bb256cacfe25fe11fb9025eb9d99806bdb3f89
G.PAUDRA <g.paudra@gmail.com>
2026-01-13 12:21:49 +0700

    buat kolom data pembelian

---
+++ app/(pembelian)/daftar-pembelian/kolom-datapembelian.tsx
@@ -0,0 +1,164 @@
+"use client"
+
+import { ColumnDef } from "@tanstack/react-table"
+import { Button } from "@/components/ui/button"
+import { ArrowUpDown} from "lucide-react"
+
+type Pembelian = {
+  id: number,
+  nama_vendor: string,
+  harga_beli: number,
+  status: string,
+  jumlahtotalharga: number,
+  biayakirim: number,
+}
+
+export const columns: ColumnDef<Pembelian>[] = [
+
+  {
+    id: "no",
+    header: () => {
+      return (
+          <div className="text-center w-1.5">No.</div>
+      )
+    },
+    size: 10,
+    cell: ({ row, table }) => {
+
+      const sortedRows = table.getSortedRowModel().flatRows
+      const index =
+        sortedRows.findIndex((r) => r.id === row.id)
+      return <p className="text-center">{index + 1}.</p>
+    },
+    enableSorting: false,
+  },
+   {
+    accessorKey: "kode",
+    size:150,
+    minSize:100,
+    header: ({ column }) => (
+      <div>
+        < Button variant="ghost" onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        className="h-8 px-2"
+      >
+        Kode Pembelian
+        <ArrowUpDown className="ml-2 h-4 w-4" />
+      </Button>
+
+      </div>
+      
+    ),
+    cell: ({ row }) => (
+      <div className="capitalize pl-2">
+        {row.getValue("kode")}
+      </div>
+    ),
+  },
+  
+  {
+    accessorKey: "nama_vendor",
+    size:200,
+    minSize:150,
+    header: ({ column }) => (
+      <div>
+        < Button variant="ghost" onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        className="h-8 px-2"
+      >
+        Vendor / Distributor
+        <ArrowUpDown className="ml-2 h-4 w-4" />
+      </Button>
+
+      </div>
+      
+    ),
+    cell: ({ row }) => (
+      <div className="capitalize pl-2">
+        {row.getValue("nama_vendor")}
+      </div>
+    ),
+  },
+  {
+    accessorKey: "status",
+    size:50,
+    minSize:32,
+    header: ({ column }) => (
+      <div>
+          < Button variant="ghost" onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          className="h-8 px-2"
+        >
+          Status
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      </div>
+      
+    ),
+    cell: ({ row }) => (
+      <div className="capitalize pl-2">
+        {row.getValue("status")}
+      </div>
+    ),
+  },
+
+  {
+    accessorKey: "jumlahtotalharga",
+    size:50,
+    minSize:32,
+    header: ({ column }) => {
+      return (
+        <div>
+          <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Jumlah Total
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+        </div>
+        
+      )
+    },
+    cell: ({ row }) => {
+      const harga = parseFloat(row.getValue("jumlahtotalharga"))
+      
+      // Format ke Rp
+      const formatted = new Intl.NumberFormat("id-ID", {
+        style: "currency",
+        currency: "IDR",
+        minimumFractionDigits: 0,
+      }).format(harga)
+
+      return <div className="text-left font-medium pl-2">{formatted}</div>
+    },
+  },
+  {
+    accessorKey: "biayakirim",
+    size:50,
+    minSize:32,
+    header: ({ column }) => {
+      return (
+        <div>
+          <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Biaya Kirim
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+        </div>
+        
+      )
+    },
+    cell: ({ row }) => {
+      const harga = parseFloat(row.getValue("biayakirim"))
+      
+      // Format ke Rp
+      const formatted = new Intl.NumberFormat("id-ID", {
+        style: "currency",
+        currency: "IDR",
+        minimumFractionDigits: 0,
+      }).format(harga)
+
+      return <div className="text-left font-medium pl-2">{formatted}</div>
+    },
+  },
+]
