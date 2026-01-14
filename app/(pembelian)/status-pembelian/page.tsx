40d1348844cb68d06392d2914227f173ec254c7d
G.PAUDRA <g.paudra@gmail.com>
2026-01-14 08:16:11 +0700

    halaman sudah berfungsi

--- app/(pembelian)/status-pembelian/page.tsx
+++ app/(pembelian)/status-pembelian/page.tsx
@@ -1,11 +1,10 @@
 import { AppSidebar } from "@/components/app-sidebar"
-import { ChartAreaInteractive } from "@/components/chart-area-interactive"
-import { SectionCards } from "@/components/section-cards"
 import { SiteHeader } from "@/components/site-header"
 import {
   SidebarInset,
   SidebarProvider,
 } from "@/components/ui/sidebar"
+import TabelStatus from "./tabel-status"
 
 export default function StatusPembelian() {
   return (
@@ -25,7 +24,9 @@ export default function StatusPembelian() {
             <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
               <div className="px-4 lg:px-6">
                 <h1 className="text-2xl font-bold">Status Pembelian Barang</h1>
-                
+                <div className="mt-3">
+                  <TabelStatus/>
+                </div>
               </div>
             </div>
           </div>
