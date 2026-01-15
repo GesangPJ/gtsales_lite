0bf7966a4ee9173802540097dbcb0185b71cd718
G.PAUDRA <g.paudra@gmail.com>
2026-01-15 11:11:51 +0700

    update schema

---
+++ prisma/migrations/20260115023759_update_transaksi/migration.sql
@@ -0,0 +1,28 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `diskon_n` on the `Transaksi` table. All the data in the column will be lost.
+  - You are about to drop the column `diskon_p` on the `Transaksi` table. All the data in the column will be lost.
+
+*/
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Transaksi" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "kode" TEXT NOT NULL,
+    "status" TEXT NOT NULL DEFAULT 'LUNAS',
+    "pelanggan" TEXT,
+    "metode" TEXT NOT NULL DEFAULT 'CASH',
+    "diskon" INTEGER,
+    "jumlahTotal" INTEGER NOT NULL,
+    "keterangan" TEXT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+INSERT INTO "new_Transaksi" ("createdAt", "id", "jumlahTotal", "keterangan", "kode", "metode", "pelanggan", "status", "updatedAt") SELECT "createdAt", "id", "jumlahTotal", "keterangan", "kode", "metode", "pelanggan", "status", "updatedAt" FROM "Transaksi";
+DROP TABLE "Transaksi";
+ALTER TABLE "new_Transaksi" RENAME TO "Transaksi";
+CREATE UNIQUE INDEX "Transaksi_kode_key" ON "Transaksi"("kode");
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
